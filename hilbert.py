6ad4d2b43a74ae46ab48504bc938c3d076eb45dc
Caglar <ca9lar@gmail.com>
2012-05-22 19:35:18 -0400

    Object orientified the code.

--- hilbert.py
+++ hilbert.py
@@ -1,48 +1,53 @@
 import sys, math
 import numpy as np
 
-data = np.zeros((256, 256))
-edges = []
+class HilbertCurves(object):
+    def __init__(self, xi, yj, level=10):
+        self.data = np.zeros((xi, yj))
+        self.edges = []
+        self.level = level
 
-def make_line(edges, X, Y):
+    def make_line(self, edges, X, Y):
 #    import pudb; pudb.set_trace()
-    if edges:
-        i, j = edges[-1]
-        xstart = int(min(i, X))
-        xend = int(max(i, X))
-        ystart = int(min(j, Y))
-        yend = int(max(j, Y))
-        if xstart == xend:
-            for y in xrange(ystart, yend):
-                data[xstart][y] = 1
-        if ystart == yend:
-            for x in xrange(xstart, xend):
-                data[x][ystart] = 1
-def hilbert_curves(xi, yj, level):
-    if xi != yj:
-        raise Exception("must be square.")
-    elif (2**level >= xi):
-        raise Exception("xi should be less then the power of two.")
-    else:
-        hilbert(0.0, 0.0, xi, 0.0, 0.0, yj, level)
+        if edges:
+            i, j = edges[-1]
+            xstart = int(min(i, X))
+            xend = int(max(i, X))
+            ystart = int(min(j, Y))
+            yend = int(max(j, Y))
+            if xstart == xend:
+                for y in xrange(ystart, yend):
+                    self.data[xstart][y] = 1
+            if ystart == yend:
+                for x in xrange(xstart, xend):
+                    self.data[x][ystart] = 1
+    def gen_hilbert_curve(self, xi, yj, level=-1):
+        if level == -1:
+            level = self.level
+        if xi != yj:
+            raise Exception("must be square.")
+        elif (2**level >= xi):
+            raise Exception("xi should be less then the power of two.")
+        else:
+            self.hilbert(0.0, 0.0, xi, 0.0, 0.0, yj, level)
 
-def hilbert(x0, y0, xi, xj, yi, yj, n):
-    if n <= 0:
-        X = x0 + (xi + yi)/2
-        Y = y0 + (xj + yj)/2
-        # Merge the dots
-        make_line(edges, X, Y)
-        edges.append((X, Y))
-    else:
-        hilbert(x0,               y0,               yi/2, yj/2, xi/2, xj/2, n - 1)
-        hilbert(x0 + xi/2,        y0 + xj/2,        xi/2, xj/2, yi/2, yj/2, n - 1)
-        hilbert(x0 + xi/2 + yi/2, y0 + xj/2 + yj/2, xi/2, xj/2, yi/2, yj/2, n - 1)
-        hilbert(x0 + xi/2 + yi,   y0 + xj/2 + yj,  -yi/2,-yj/2,-xi/2,-xj/2, n - 1)
+    def hilbert(self, x0, y0, xi, xj, yi, yj, n):
+        if n <= 0:
+            X = x0 + (xi + yi)/2
+            Y = y0 + (xj + yj)/2
+            # Merge the dots
+            self.make_line(self.edges, X, Y)
+            self.edges.append((X, Y))
+        else:
+            self.hilbert(x0,               y0,               yi/2, yj/2, xi/2, xj/2, n - 1)
+            self.hilbert(x0 + xi/2,        y0 + xj/2,        xi/2, xj/2, yi/2, yj/2, n - 1)
+            self.hilbert(x0 + xi/2 + yi/2, y0 + xj/2 + yj/2, xi/2, xj/2, yi/2, yj/2, n - 1)
+            self.hilbert(x0 + xi/2 + yi,   y0 + xj/2 + yj,  -yi/2,-yj/2,-xi/2,-xj/2, n - 1)
 
 if __name__=="__main__":
-    hilbert_curves(256, 256, 7)
+    hcurves = HilbertCurves(256, 256, 7)
     import pylab
     pylab.axis("off")
     pylab.gray()
-    pylab.imshow(data)
+    pylab.imshow(hcurves.data)
     pylab.show()
